75057f4d3fdd40ea3e37ddef08cae0982deaa6ca
Ben van Eekelen <benvaneekelen674@gmail.com>
2017-05-31 22:35:19 +0200

    let's edit the style

--- app/src/main/java/nl/ben_ey/bridge/MainActivity.java
+++ app/src/main/java/nl/ben_ey/bridge/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_profie:
+                    mTextMessage.setText(R.string.title_profile);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_bubbles:
+                    mTextMessage.setText(R.string.title_bubbles);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_chat:
+                    mTextMessage.setText(R.string.title_chat);
                     return true;
             }
             return false;
