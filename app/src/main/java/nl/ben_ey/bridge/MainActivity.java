5aa01334677ee6c5bbb4a3dc88110b8e5e9db5b2
Ben van Eekelen <benvaneekelen674@gmail.com>
2017-06-01 00:29:59 +0200

    added my own launcher icon

--- app/src/main/java/nl/ben_ey/bridge/MainActivity.java
+++ app/src/main/java/nl/ben_ey/bridge/MainActivity.java
@@ -1,23 +1,29 @@
 package nl.ben_ey.bridge;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    public BottomNavigationItemView mBubblesBtn;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
             switch (item.getItemId()) {
-                case R.id.navigation_profie:
+                case R.id.navigation_profile:
                     mTextMessage.setText(R.string.title_profile);
                     return true;
                 case R.id.navigation_bubbles:
