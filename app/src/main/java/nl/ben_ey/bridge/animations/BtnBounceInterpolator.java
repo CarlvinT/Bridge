1681553370c57c4e5dac1888a9155cf0f0702b08
Ben van Eekelen <benvaneekelen674@gmail.com>
2017-06-02 17:27:12 +0200

    so now the bubble comes in animated, still have to fix the text

---
+++ app/src/main/java/nl/ben_ey/bridge/animations/BtnBounceInterpolator.java
@@ -0,0 +1,20 @@
+package nl.ben_ey.bridge.animations;
+
+/**
+ * Created by Ben on 2-6-2017.
+ */
+
+public class BtnBounceInterpolator implements android.view.animation.Interpolator {
+    double mAmplitude = 1;
+    double mFrequency = 10;
+
+    public BtnBounceInterpolator(double amplitude, double frequency) {
+        mAmplitude = amplitude;
+        mFrequency = frequency;
+    }
+
+    public float getInterpolation(float time){
+        return (float) (-1 * Math.pow(Math.E, -time / mAmplitude) *
+                Math.cos(mFrequency * time) + 1);
+    }
+}
--- app/src/main/java/nl/ben_ey/bridge/fragments/BubblesFragment.java
+++ app/src/main/java/nl/ben_ey/bridge/fragments/BubblesFragment.java
@@ -1,12 +1,18 @@
 package nl.ben_ey.bridge.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import nl.ben_ey.bridge.R;
+import nl.ben_ey.bridge.animations.BtnBounceInterpolator;
 
 /**
  * Created by Ben on 1-6-2017.
@@ -14,9 +20,37 @@ import nl.ben_ey.bridge.R;
 
 public class BubblesFragment extends Fragment {
 
+    Activity referenceActivity;
+    View parentHolder;
+
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bubbles, container, false);
+
+        // Set the reference activity and inflate the layout for this fragment
+        referenceActivity = getActivity();
+        parentHolder = inflater.inflate(R.layout.fragment_bubbles, container, false);
+
+        // Get the centre bubble and the text
+        ImageButton centreBtn = (ImageButton) parentHolder.findViewById(R.id.CentreBubble);
+        TextView yourName = (TextView) parentHolder.findViewById(R.id.UserName);
+
+        // Set up the animation
+        final Animation centreBtnEnter = AnimationUtils.loadAnimation(referenceActivity,
+                R.anim.bubble_bounce);
+
+        // Use bounce interpolator with amplitude 0.2 and frequency 20
+        //  Higher amplitude means more pronounced bounces
+        //  Higher frequency means more wobbles during the animation
+        BtnBounceInterpolator interpolator = new BtnBounceInterpolator(0.2, 20);
+
+        // Couple the interpolator to the animation
+        centreBtnEnter.setInterpolator(interpolator);
+
+        // Couple the animation to the button and the text
+        centreBtn.startAnimation(centreBtnEnter);
+        yourName.startAnimation(centreBtnEnter);
+
+        // Return the inflated layout
+        return parentHolder;
     }
 }
