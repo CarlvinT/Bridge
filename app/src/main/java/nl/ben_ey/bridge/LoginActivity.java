5d7de7b5d593e8baf06dc089403727bcbad79c89
Ben van Eekelen <benvaneekelen674@gmail.com>
2017-07-13 13:02:30 +0200

    Login logout works

--- app/src/main/java/nl/ben_ey/bridge/LoginActivity.java
+++ app/src/main/java/nl/ben_ey/bridge/LoginActivity.java
@@ -1,13 +1,94 @@
 package nl.ben_ey.bridge;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText mEmail;
+    private EditText mPassword;
+    private Intent i;
+    FirebaseUser user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = (EditText) findViewById(R.id.name_input);
+        mPassword = (EditText) findViewById(R.id.password_input);
+
+        i = new Intent(this, MainActivity.class);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user == null) {
+            Toast.makeText(LoginActivity.this, "not signed in",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(LoginActivity.this, "signed in",
+                    Toast.LENGTH_LONG).show();
+
+            startActivity(i);
+        }
+    }
+
+    public void loginUser(View view) {
+
+
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password))
+        {
+            Toast.makeText(LoginActivity.this, "Input Email and password", Toast.LENGTH_LONG).show();
+        }
+        else{
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                user = mAuth.getCurrentUser();
+                                // getUserInformation();
+                                Toast.makeText(LoginActivity.this, "Welcome login succesfull ",
+                                        Toast.LENGTH_LONG).show();
+
+                                startActivity(i);
+
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+                    });
+
+        }
+
+
     }
 }
--- app/src/main/java/nl/ben_ey/bridge/fragments/ProfileFragment.java
+++ app/src/main/java/nl/ben_ey/bridge/fragments/ProfileFragment.java
@@ -1,11 +1,22 @@
 package nl.ben_ey.bridge.fragments;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import nl.ben_ey.bridge.LoginActivity;
+import nl.ben_ey.bridge.MainActivity;
 import nl.ben_ey.bridge.R;
 
 /**
@@ -14,10 +25,46 @@ import nl.ben_ey.bridge.R;
 
 public class ProfileFragment extends Fragment {
 
+    private FragmentActivity listener;
+    private Button mLogoutBtn;
+    private Intent i;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof Activity) {
+            this.listener = (FragmentActivity) context;
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
 
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mLogoutBtn = (Button) listener.findViewById(R.id.logout_button);
+        i = new Intent(listener, LoginActivity.class);
+
+        mLogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+
+    }
+
+
+    private void logoutUser(){
+        FirebaseAuth.getInstance().signOut();
+        Toast.makeText(listener, "Signed out", Toast.LENGTH_LONG).show();
+
+        startActivity(i);
+
     }
 }
